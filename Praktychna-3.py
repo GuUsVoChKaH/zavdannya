4b0d93fe3e9b3b8cc7503feda5a53ff2530e5948
SOROKA PAVLO <pavlosoroka30@gmail.com>
2024-11-03 22:10:48 +0200

    Add files via upload

---
+++ Praktychna-3.py
@@ -0,0 +1,19 @@
+Slovnik_type_znachen = {
+    "name": ("Pavlo"),
+    "age": (16),
+    "contact_information": {
+        "email_adres": ("pavlosoroka30gmail.com"),
+        "city": ("Lutsk"),
+        "country": ('Ukraine'),
+        "adress": ('Konyakina_5a')
+    },
+    "Is_student": (True)
+}
+type_dict = {}  
+for key, value in Slovnik_type_znachen.items():
+    if type(value) == dict:
+        for k1, v1 in value.items():
+            type_dict[k1] = type(v1)
+    else:
+        type_dict[key] = type(value) 
+print(type_dict)
